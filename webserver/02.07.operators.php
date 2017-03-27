311cf72ec47ee3040044c05bb76632687c7e398d
Serhat Cevikel <serhatcevikel@yahoo.com>
2017-03-27 23:03:58 +0300

    commit

--- webserver/02.07.operators.php
+++ webserver/02.07.operators.php
@@ -15,33 +15,28 @@
              
             // add 
             $sum = $x + $y; 
-            <p>
-                echo "$x + $y = $sum\n";
-            </p>
+            echo "$x + $y = $sum\n";
+            <p></p>
              
             // subtract 
             $diff = $x - $y; 
-            <p>
                 echo "$x - $y = $diff\n";
-            </p> 
+            <p></p> 
              
             // multiply 
             $product = $x * $y; 
-            <p>
                 echo "$x * $y = $product\n";
-            </p> 
+            <p></p> 
              
             // divide and get quotient 
             $quotient = $x / $y; 
-            <p>
                 echo "$x / $y = $quotient\n";
-            </p> 
+            <p></p> 
              
             // divide and get modulus 
             $modulus = $x % $y; 
-            <p>
                 echo "$x % $y = $modulus\n";
-            </p> 
+            <p></p> 
         ?>
     </body>
 </html>
